2f868bad1eb6bff64edc111b58347948cd7964f2
akohinmiogbegbile <akohinmi90@gmail.com>
2023-08-13 13:52:44 -0700

    AirBnB Group Project

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Module for the entry point of the command interpreter.
+"""Module for the entry point of the command interpreter."""
 
 import cmd
 from models.base_model import BaseModel
@@ -10,12 +10,12 @@ import json
 
 class HBNBCommand(cmd.Cmd):
 
-    #This is the Class for the command interpreter.
+    """This is the Class for the command interpreter."""
 
     prompt = "(hbnb) "
 
     def default(self, line):
-        #Catch commands if nothing else matches then.
+        """Catch commands if nothing else matches then."""
         # print("DEF:::", line)
         self._precmd(line)
 
